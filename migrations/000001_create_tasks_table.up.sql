a5c6102e20bd403805a4bd7ebeda9f43a2985fe5
knapptan <nikitas@live.ru>
2025-05-06 22:53:19 +0300

    add migrations

---
+++ migrations/000001_create_tasks_table.up.sql
@@ -0,0 +1,10 @@
+-- CREATE DATABASE todo;
+
+CREATE TABLE tasks (
+id SERIAL PRIMARY KEY,
+title TEXT NOT NULL,
+description TEXT,
+status TEXT CHECK (status IN ('new', 'in_progress', 'done')) DEFAULT 'new',
+created_at TIMESTAMP DEFAULT NOW(),
+updated_at TIMESTAMP DEFAULT NOW()
+);
